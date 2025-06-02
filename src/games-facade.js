e0489a8388f3fa3679b54e777a129e7bc8ee96d1
henriqueFon <henriquearaujo091@gmail.com>
2025-06-02 16:15:24 -0300

    transforming cards in link

--- src/games-facade.js
+++ src/games-facade.js
@@ -11,16 +11,19 @@ export async function renderGames() {
         const card = document.createElement('div');
         card.className = 'game-card';
 
+        // Cria um link que envolve todo o conteúdo do card
         card.innerHTML = `
-        <img src="${game.image}" alt="${game.name}">
-        <div class="game-info">
-            <h2>${game.name}</h2>
-            <div class="genre">${game.genre}</div>
-            <div class="score">Nota média: ${game.avarage_score ?? game.average_score}</div>
-        </div>
+        <a href="detalhes.html?id=${game.id}" style="text-decoration: none; color: inherit;">
+            <img src="${game.image}" alt="${game.name}">
+            <div class="game-info">
+                <h2>${game.name}</h2>
+                <div class="genre">${game.genre}</div>
+                <div class="score">Nota média: ${game.avarage_score ?? game.average_score}</div>
+            </div>
+        </a>
         `;
-        
+
         recentGames.appendChild(card);
     });
-    
 }
+
