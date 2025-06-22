c8660483e06f0d4a918e6e9ef45608a0abaf8090
GTgame2 <tb4296995@gmail.com>
2025-06-22 16:23:13 -0300

    Odeio GIT, dados do setup e cadastro.

--- src/Geral.js
+++ src/Geral.js
@@ -1,35 +1,29 @@
-const dados={
-    usuarios: [
-        {id:1, nome: "Davi Jonas", login: "Dova", senha:"abc123", email: "dovajonas@gmail.com"},
-        {id:2, nome: "São Pedro", login: "Chaveiro", senha:"abc123", email: "saopedro@gmail.com"}
-    ]
-}
-
-//variavel quem mantem os dados do usuario logado
-let usuario = null;
-
-function checkLoggedUser(){
-    const usuarioSTR = sessionStorage.getItem('usuario')
-    if(!usuarioSTR){
-        location.href = 'Login.html'
-    }
-    usuario = JSON.parse(usuarioSTR)
-    return true
-}
-
-function loginUser(login, password){
-    const usuarioOBJ = dados.usuarios.find(elem => (elem.login === login) && (elem.senha === password))
-
-    if (!usuarioOBJ){
-        return false
-    }
-    else {
-        sessionStorage.setItem('usuario', JSON.stringify(usuarioOBJ))
-        return true
+export async function apiGet(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Erro ao buscar dados de ${url}`);
+        }
+        return await response.json();
+    } catch (error) {
+        throw new Error(error.message);
     }
 }
 
-function logouUser(){
-    sessionStorage.clear();
-    location.href="Login.html"
+export async function login(username, password) {
+    try {
+        const userCredentials = await apiGet("http://localhost:3001/users");
+        const findUser = userCredentials.find(user => user.username === username);
+        if (findUser) {
+            if (findUser.password === password) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return "Usuário não encontrado.";
+        }
+    } catch (error) {
+        return `Erro ao buscar usuários: ${error.message}`;
+    }
 }
